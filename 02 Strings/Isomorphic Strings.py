e8d4ad329eb8fde5a33cbebcccbb692dc53343ff
Fhiroz Khan Shaik <161280861+FHIROZ@users.noreply.github.com>
2025-11-02 08:02:07 +0530

    Add files via upload

---
+++ 02 Strings/Isomorphic Strings.py
@@ -0,0 +1,20 @@
+def isIsomorphic(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+    
+    map_st = {}
+    map_ts = {} 
+    
+    for ch1, ch2 in zip(s, t):
+        if ch1 in map_st and map_st[ch1] != ch2:
+            return False
+        if ch2 in map_ts and map_ts[ch2] != ch1:
+            return False
+        map_st[ch1] = ch2
+        map_ts[ch2] = ch1
+    return True
+
+print(isIsomorphic("egg", "add"))     
+print(isIsomorphic("foo", "bar"))     
+print(isIsomorphic("paper", "title")) 
+print(isIsomorphic("ab", "aa"))       
